90ebf4943c2716a1c4d3b943c37c364d7d3ea361
wlMalk <wm.wexem@gmail.com>
2016-02-21 20:10:05 -0600

    More interfaces for database driver

--- database/database.go
+++ database/database.go
@@ -8,6 +8,27 @@ type Driver interface {
 	Queryer
 	QueryRower
 	Execer
+	Close() error
+	Ping() error
+	Prepare(string) (*sql.Stmt, error)
+	SetMaxIdleConns(int)
+	SetMaxOpenConns(int)
+}
+
+type Transaction interface {
+	Queryer
+	QueryRower
+	Execer
+	Rollback() error
+	Commit() error
+	Prepare(string) (*sql.Stmt, error)
+	Stmt(*sql.Stmt) *sql.Stmt
+}
+
+type Model interface {
+}
+
+type Slice interface {
 }
 
 type Queryer interface {
@@ -35,7 +56,7 @@ type Rower interface {
 
 type Result interface {
 	sql.Result
-	InsertIds() ([]interface{}, error)
+	// InsertIds() ([]interface{}, error)
 }
 
 type Tabler interface {
@@ -76,9 +97,23 @@ func (t *Table) Column(a string) string {
 	return t.columns[a]
 }
 
+func (t *Table) Columns(a ...string) []string {
+	var b []string
+
+	for _, c := range a {
+		b = append(b, t.columns[c])
+	}
+
+	return b
+}
+
 type DB struct {
 	*sql.DB
 	name string
+}
+
+type Tx struct {
+	*sql.Tx
 }
 
 func New() *DB {
@@ -87,4 +122,36 @@ func New() *DB {
 
 func (db *DB) SetName(name string) {
 	db.name = name
+}
+
+func (db *DB) Query(query string, args ...interface{}) (Rower, error) {
+	return db.DB.Query(query, args...)
+}
+
+func (db *DB) Begin() (Transaction, error) {
+	xtx, err := db.DB.Begin()
+	if err != nil {
+		return nil, err
+	}
+	return &Tx{xtx}, err
+}
+
+func (db *DB) QueryRow(query string, args ...interface{}) (Rower, error) {
+	return db.DB.Query(query, args...)
+}
+
+func (db *DB) Exec(query string, args ...interface{}) (Result, error) {
+	return db.DB.Exec(query, args...)
+}
+
+func (tx *Tx) Query(query string, args ...interface{}) (Rower, error) {
+	return tx.Tx.Query(query, args...)
+}
+
+func (tx *Tx) QueryRow(query string, args ...interface{}) (Rower, error) {
+	return tx.Tx.Query(query, args...)
+}
+
+func (tx *Tx) Exec(query string, args ...interface{}) (Result, error) {
+	return tx.Tx.Exec(query, args...)
 }
